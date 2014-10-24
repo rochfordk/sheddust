c329363e30163f03a6ea707eed4f52727fc4aaf7
rochfordk <keith.rochford@gmail.com>
2014-10-24 11:08:08 +0100

    Updated comments

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,16 +3,7 @@
 
 
 /*
-  LiquidCrystal Library - Hello World
- 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD
- and shows the time.
- 
+
   The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
@@ -25,21 +16,9 @@
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
  
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- 
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
 
-// include the library code:
+// include the LCD library code:
 #include <LiquidCrystal.h>
 
 //#define n 2
@@ -83,8 +62,6 @@ void setup() {
   
   //TODO - iterate over the machine array setting the pin mode for the sensor inputs.
 
-  
-  //machines = { {1, "Planer", 800, 5},{1, "Bandsaw", 800, 5}};
 }
 
 void loop() {
